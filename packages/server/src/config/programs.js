95d0040e62ba58225d1ce60f2f9a865a789e0626
Suraj Shrestha <syuraj@gmail.com>
2021-03-27 15:29:08 -0400

    add publisher to program

--- packages/server/src/config/programs.js
+++ packages/server/src/config/programs.js
@@ -5,6 +5,11 @@ SourceConfig.forEach((pub) => {
 	pub.pages.forEach((pgm) => {
 		pgm.id = pgm.programId
 		pgm.title = pgm.program
+		pgm.publisher = {
+			id: pub.sourceId,
+			title: pub.sourceName,
+			imageUrl: pub.imageUrl,
+		}
 		Programs.push(pgm)
 	})
 })
