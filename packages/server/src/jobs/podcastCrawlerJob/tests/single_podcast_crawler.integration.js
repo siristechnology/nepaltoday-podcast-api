b3dad55953fcf9c8e693ba030ba1914d62d1950c
Suraj Shrestha <syuraj@gmail.com>
2021-03-28 14:23:57 -0400

    fix title & description of कायाकैरन podcast

--- packages/server/src/jobs/podcastCrawlerJob/tests/single_podcast_crawler.integration.js
+++ packages/server/src/jobs/podcastCrawlerJob/tests/single_podcast_crawler.integration.js
@@ -13,9 +13,10 @@ describe('podcast-crawler', () => {
 	})
 
 	it('MiliJuli Nepali podcast can be scraped', async () => {
-		const source = SourceConfig.filter((s) => s.sourceId === 'bbcmedia' && s.pages.some((p) => p.programId === 'MiliJuliNepali'))
+		const source = SourceConfig.filter((s) => s.sourceId === 'bbcmedia' && s.pages.some((p) => p.programId === 'MiliJuliNepali'))[0]
+		source.pages = source.pages.filter((p) => p.programId === 'MiliJuliNepali')
 
-		const podcasts = await PodcastCrawler(source, { headless: true, articleUrlLength: 3 })
+		const podcasts = await PodcastCrawler([source], { headless: true, articleUrlLength: 3 })
 
 		expect(podcasts.length).toBeGreaterThan(0)
 	})
@@ -29,9 +30,19 @@ describe('podcast-crawler', () => {
 	})
 
 	it('Nepal Sandarbha podcast can be scraped', async () => {
-		const source = SourceConfig.filter((s) => s.sourceId === 'bbcmedia' && s.pages.some((p) => p.programId === 'NepalSandarbha'))
+		const source = SourceConfig.filter((s) => s.sourceId === 'bbcmedia' && s.pages.some((p) => p.programId === 'NepalSandarbha'))[0]
+		source.pages = source.pages.filter((p) => p.programId === 'NepalSandarbha')
 
-		const podcasts = await PodcastCrawler(source, { headless: true, articleUrlLength: 3 })
+		const podcasts = await PodcastCrawler([source], { headless: true, articleUrlLength: 3 })
+
+		expect(podcasts.length).toBeGreaterThan(0)
+	})
+
+	it('Kayakairan podcast can be scraped', async () => {
+		const source = SourceConfig.filter((s) => s.sourceId === 'Ujyalo' && s.pages.some((p) => p.programId === 'Kayakairan'))[0]
+		source.pages = source.pages.filter((p) => p.programId === 'Kayakairan')
+
+		const podcasts = await PodcastCrawler([source], { headless: true, articleUrlLength: 3 })
 
 		expect(podcasts.length).toBeGreaterThan(0)
 	})
